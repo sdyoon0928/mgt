839ee822787fb67b2e36d489a69e1d4d4e5b31c1
SUNGJUN94 <fiwns7942@gmail.com>
2025-09-05 17:46:05 +0900

    merge test3-1

--- login/manage.py
+++ login/manage.py
@@ -15,6 +15,8 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
+            "nvlsnvendnvldknbdb"
+            "vnsdvnosnvdsiolnslonvowndvvlsknvlosdvmwejpwb"
         ) from exc
     execute_from_command_line(sys.argv)
 
