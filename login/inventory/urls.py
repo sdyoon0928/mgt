b6a8898f1290d6b38c467bca08c6f3512d7ac19e
SUNGJUN94 <fiwns7942@gmail.com>
2025-09-05 12:58:33 +0900

    싹다 삭제

--- login/inventory/urls.py
+++
@@ -1,7 +0,0 @@
-# inventory/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inventory_home, name='inventory_home'),
-]
--- login/inventory/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
-
-def inventory_home(request):
-    return render(request, 'inventory/home.html')
